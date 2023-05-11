b47118f270ef2acca39c4dcd0a3e59b5a8d25673
RebecaLPereira <leitepereira.rebeca@gmail.com>
2023-05-11 18:30:09 -0300

    criando home

---
+++ src/paginas/home/home.tsx
@@ -0,0 +1,20 @@
+import "./style.css";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <ul id="menu">
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="index.html">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Home;

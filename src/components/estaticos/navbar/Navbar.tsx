2393b1be37e3eb84e30999d1d895a45a1b84992e
RebecaLPereira <leitepereira.rebeca@gmail.com>
2023-05-16 14:21:54 -0300

    Atualização

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,48 +1,9 @@
 import React from 'react';
-import './Navbar.css'
-import { AppBar, Button, Divider, Toolbar, Typography } from '@material-ui/core';
-import {Box} from '@mui/material';
-function Navbar() {
-    return (
-        <>
-            <AppBar position="static" className='Appbar' >
-                <Toolbar variant="dense" style={{background: "#810081", justifyContent: 'space-around'}} className='Navbar'>
-                    <Box style={{ cursor: "pointer" }} className='Navbar-Logo'>
-                        <Typography variant="h5" color="inherit" >
-                            BlogPessoal - Vinicius
-                        </Typography>
-                        
-                    </Box>
-                    
-                    <Box display="flex">
-                            
-                            <Box px={1} style={{ cursor: "pointer" }} className='NavbarLink -NavbarLink'>
-                                <Button variant="text" style={{color: "white" }}>Home</Button>
-                            </Box>
-                            
-                            <Box px={1} style={{ cursor: "pointer" }} className='NavbarLink'>
-                            <Button variant="text" style={{color: "white" }}>Postagens</Button>
-                            </Box>
-                            
-                            <Box px={1} style={{ cursor: "pointer" }} className='NavbarLink'>
-                            <Button variant="text" style={{color: "white" }}>Temas</Button>
-                            </Box>
 
-                            <Box px={1} style={{ cursor: "pointer" }} className='NavbarLink'>
-                            <Button variant="text" style={{color: "white" }}>Cadastrar Tema</Button>
-                            </Box>
-                            
-                            <Box px={1} style={{ cursor: "pointer" }} className='NavbarLink NavbarLink-'>
-                            <Button variant="text" style={{color: "white" }}>Logout</Button>
-                            </Box>
-                            
-                    </Box>
-
-                </Toolbar>
-            </AppBar>
-            
-        </>
-    )
+function Navbar (){
+    <>
+    
+    </>
 }
 
 export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./paginas/home/home";
+import Home from "./paginas/home/Home";
 ReactDOM.render(
   <React.StrictMode>
     <Home />
